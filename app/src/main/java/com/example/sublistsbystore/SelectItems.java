e1b77c474fe23ef25805682a66c207d0dd117bcc
Alex Guillen <jag4t6@gmail.com>
2021-04-07 14:14:28 -0400

    add empty methods and TODO's to allow for functionality

--- app/src/main/java/com/example/sublistsbystore/SelectItems.java
+++ app/src/main/java/com/example/sublistsbystore/SelectItems.java
@@ -2,7 +2,14 @@ package com.example.sublistsbystore;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
 
 public class SelectItems extends AppCompatActivity {
 
@@ -10,16 +17,72 @@ public class SelectItems extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_select_items);
+
+        buildTable();
     }
-//    TODO: add functionality --add to list button
 
-//    TODO: add  delete button -- individual items
+    /**
+     * creates list of items and adds items to array
+     * @param item
+     */
+    public void buildList(String item) {
+        //    TODO: add functionality
+    }
 
-//    TODO: add clear list button
+    /**
+     * inserts a new item to shopping list
+     * @param view
+     */
+    public void addItemBtnClicked(View view) {
+        EditText input = findViewById(R.id.user_text_input);
+
+        if (input.getText().toString().isEmpty()) {
+            Toast.makeText(getApplicationContext(), "Field cannot be empty!", Toast.LENGTH_SHORT).show();
+        } else {
+//            TODO: finish build table
+        }
+    }
+
+    public void nextPageButton(View view) {
+        //FIXME: replace class name for 'how many stops' page --
+        startActivity(new Intent(getApplicationContext(), null));
+    }
+
+    /**
+     * clears the list of all items
+     * @param view
+     */
+    public void clearListBtn(View view) {
+        //    TODO: add clear list button
+    }
+
+    /**
+     * refreshes list of items in display
+     */
+    public void buildTable() {
+        //      TODO: finish makeDeleteItemButton()
+    }
+
+    /**
+     * adds delete button to each item on list
+     * @return
+     */
+    private Button makeDeleteItemButton() {
+        //    TODO: add  delete button -- individual items, identify how we are going to store 'list of items'
+        return null;
+    }
+
+    private TextView makeTV(String item) {
+        //    TODO: set resource file for text size and padding
+        return null;
+    }
+
 
 //    TODO: research 'bottom nav bar' design--> [<]Backward [Home/CurrentList] Forward[>]
 
 //    TODO: create intent that leads to how many stops page
+
+//    TODO: Implement popup after user clicks 'removeBtn' or 'clearListBtn'
 
 
 }
