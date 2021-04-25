a4f1d82f8c5d24c2f11ea9f7e208dbabad2f6659
Alex Guillen <jag4t6@gmail.com>
2021-04-25 16:31:16 -0400

    add quantity control to item row

--- app/src/main/java/com/example/sublistsbystore/SelectItemsActivity.java
+++ app/src/main/java/com/example/sublistsbystore/SelectItemsActivity.java
@@ -5,9 +5,12 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.view.ViewGroup;
 import android.view.inputmethod.InputMethodManager;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.LinearLayout;
+import android.widget.NumberPicker;
 import android.widget.TableLayout;
 import android.widget.TableRow;
 import android.widget.TextView;
@@ -21,12 +24,13 @@ import java.util.List;
 public class SelectItemsActivity extends AppCompatActivity {
 
     private List<Item> itemList = new ArrayList<>();
+    float scale;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_select_items);
-
+        scale = getApplicationContext().getResources().getDisplayMetrics().density;
         buildItemTable();
     }
 
@@ -87,12 +91,15 @@ public class SelectItemsActivity extends AppCompatActivity {
      */
     public void buildItemTable() {
         TableLayout table = findViewById(R.id.current_items_table);
+
+        table.setStretchAllColumns(true);
         table.removeAllViews(); // clear existing items
 
 //      for each item on list, create a delete button and add textView
         for (Item item : itemList) {
             TableRow row = new TableRow(this.getApplicationContext());
             row.addView(makeDeleteItemButton(item));
+            row.addView(makeQuantityInput(item));
             row.addView(makeTV(item.getName()));
             table.addView(row);
         }
@@ -123,6 +130,52 @@ public class SelectItemsActivity extends AppCompatActivity {
         listView.setTextSize(16);
         listView.setPadding(1, 0, 0, 0);
         return listView;
+    }
+
+    private LinearLayout makeQuantityInput(Item item) {
+        LinearLayout container = new LinearLayout(this.getApplicationContext());
+//        container.setOrientation(LinearLayout.VERTICAL);
+
+        TextView np = new TextView(this.getApplicationContext());
+        np.setText(item.getQuantity() + "");
+
+        Button inc = new Button(this.getApplicationContext());
+        inc.setText("+");
+        inc.setOnClickListener(v -> {
+            item.setQuantity(item.getQuantity() + 1);
+            np.setText(item.getQuantity() + "");
+        });
+        Button dec = new Button(this.getApplicationContext());
+        dec.setText("-");
+        dec.setOnClickListener(v -> {
+            if (item.getQuantity() > 1) {
+
+                item.setQuantity(item.getQuantity() - 1);
+                np.setText(item.getQuantity() + "");
+            }
+        });
+
+
+        container.addView(dec);
+        container.addView(np);
+        container.addView(inc);
+
+
+        // credit: https://stackoverflow.com/a/5255256
+        int pixels = (int) (41 * scale + 0.5f);
+
+        // TODO how to make increment/decrement/remove buttons narrower programmatically
+        // TODO or is there a better way to be building each row? (TableRow as Fragment? Separate Class?)
+        LinearLayout.LayoutParams params = new LinearLayout.LayoutParams(pixels, pixels);
+        params.weight = 0.5f;
+        inc.setMaxWidth(pixels);
+        inc.setWidth(pixels);
+        inc.setLayoutParams(params);
+        dec.setMaxWidth(pixels);
+        dec.setWidth(pixels);
+        dec.setLayoutParams(params);
+
+        return container;
     }
 
 
