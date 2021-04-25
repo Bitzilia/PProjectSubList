0e3f9dd3c2839603d03fb0898e09a435c9a723c3
Alex Guillen <jag4t6@gmail.com>
2021-04-25 16:30:54 -0400

    add quantity to item

--- app/src/main/java/com/example/sublistsbystore/Item.java
+++ app/src/main/java/com/example/sublistsbystore/Item.java
@@ -13,6 +13,7 @@ public class Item {
     @PrimaryKey
     private int uid;
     private String name;
+    private int quantity = 1;
 
     public Item() {
     }
@@ -26,11 +27,28 @@ public class Item {
         this.setName(name);
     }
 
+    public Item(int uid, String name, int quantity) {
+        this.uid = uid;
+        this.setName(name);
+        this.setQuantity(quantity);
+    }
+
     public String getName() {
         return this.name;
     }
 
     public void setName(String name) {
         this.name = name;
+    }
+
+    public int getQuantity() {
+        return this.quantity;
+    }
+
+    public void setQuantity(int quantity) {
+        if (quantity < 1) {
+            throw new IllegalArgumentException("Cannot set item quantity lower than 1");
+        }
+        this.quantity = quantity;
     }
 }
